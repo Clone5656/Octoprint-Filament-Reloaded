a31f6118867da7b9751e4d2121007671a5c557cc
Clone5656 <kenchunhanlin@gmail.com>
2017-03-05 21:42:24 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,13 +21,13 @@ plugin_version = "1.0.1"
 plugin_description = """A revamped and rewritten filament monitor that pauses the print when your filament runs out"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Connor Huffine"
+plugin_author = "Ken Lin"
 
 # The plugin's author's mail address.
-plugin_author_email = "chuffine@gmail.com"
+plugin_author_email = "kenchunhanlin@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/kontakt/Octoprint-Filament-Reloaded"
+plugin_url = "https://github.com/Clone5656/Octoprint-Filament-Reloaded"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
